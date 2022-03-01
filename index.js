ff21ec1ec6e3575484fad07f099f536f81b39c58
Amer Mall <amer@icloud.com>
2022-02-28 22:52:16 -0500

    commit tree 98ea99213b746e6fec0273ab5ab6444b2a97b168

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+// const main = document.querySelector('main');
+// main.remove();
+
+// const newHeader = document.createElement('h1');
+// document.body.append(newHeader);
+// newHeader.id = "victory";
+
+// newHeader.innerHTML = "Amer Mall is the champion!";
+// newHeader.className = "victory";
+// test not working....
+
+
+// Using solution...
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Amer Mall is the champion!";
+newHeader.className = "victory";
